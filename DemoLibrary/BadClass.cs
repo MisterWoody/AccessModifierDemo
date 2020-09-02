a54f2e935b85141849e9dcd297f2e02e0e8e59f8
Tony Watkins <tony@llifion.net>
2020-09-02 19:34:18 +0100

    Using a propfull snippet and private backed property to control visibility and access.

--- DemoLibrary/BadClass.cs
+++ DemoLibrary/BadClass.cs
@@ -9,5 +9,20 @@ namespace DemoLibrary
     public class BadClass
     {
         public string creditCardNumber; // Never do this!
+
+
+        // propfull allows protection and encapsulation of a classes properties.
+        // Do not declare _ssn as public though - as this is back to full visibility
+
+        private string _ssn;
+
+        public string SSN
+        {
+            get { 
+                return "***-**-1234"; 
+            }
+            set { _ssn = value; }
+        }
+
     }
 }
